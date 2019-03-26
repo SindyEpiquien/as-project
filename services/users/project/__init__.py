1688cb092d324a7a13c24b8ffb00c90130fa4b70
Sindy <sindyepiquien@upeu.edu.pe>
2019-03-25 21:59:02 -0500

    Cambios

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,16 +1,43 @@
 # services/users/project/__init__.py
 
-from flask import Flask, jsonify
+import os
 
-# instanciamos la app
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy     # nuevo
+
+# Istanciado la app
 app = Flask(__name__)
 
-#estableciendo confiracion
+# estableciendo configuracion
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# instanciado la db
+db = SQLAlchemy(app)    # nuevo
+
+
+# modelo
+class User(db.Model):   # nuevo
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+# Establecer configuracion
 app.config.from_object('project.config.DevelopmentConfig')
+
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
-   return jsonify({
-       'status':'success',
-       'message': 'pong'
-   })
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+})
+
+#docker-compose -f docker-compose-dev.yml logs
+#ya sea en modo desarrollo, prueba o producción
